4dd86d88ecf537c4dc8047bc9d298fdb75bf45f3
Lucky Singh <alucky0112@gmail.com>
2026-01-22 20:52:37 +0530

    Fix server URL assignment for production environment

--- Frontend/environment.js
+++ Frontend/environment.js
@@ -1,7 +1,8 @@
 let IS_PORD = true;
 
 const server = IS_PORD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://apna-video-call-backend-9bl8.onrender.com" :
+    
     "http://localhost:8000";
 
 export default server;
